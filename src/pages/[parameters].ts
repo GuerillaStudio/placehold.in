7b4b40c474623d0090d971297b1a479a991af534
wryk <wryk@wryk.moe>
2023-07-12 23:49:00 +0200

    add empty getStaticPaths

--- src/pages/[parameters].ts
+++ src/pages/[parameters].ts
@@ -7,6 +7,10 @@ import { ENV, SUPPORTED_FORMATS } from "../env"
 import { Placeholder } from "../components/PlaceholderImage"
 import sharp from "sharp"
 
+export async function getStaticPaths() {
+	return []
+}
+
 export const get: APIRoute = async ({params, request}) => {
 	const literalResult = literalParametersSchema.safeParse(params.parameters)
 
