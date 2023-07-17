b564f7384aae09058dd48aca46bc92645d247310
wryk <wryk@wryk.moe>
2023-07-17 20:32:55 +0200

    split build

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,34 +6,37 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  // site: "https://placehold.in",
   output: "server",
   adapter: netlify(),
   integrations: [tailwind(), react()],
-  // site: "https://placehold.in"
-  vite: {
-    plugins: [
-      // rawFonts([".woff"])
-    ]
-  }
+  build: {
+    split: true
+  },
+  // vite: {
+  //   plugins: [
+  //     rawFonts([".woff"])
+  //   ]
+  // },
 });
 
-// adapted from mattjennings
-// https://github.com/mattjennings/mattjennings.io/blob/master/vite.config.js
-function rawFonts(ext) {
-  return {
-    name: 'vite-plugin-raw-fonts',
-    resolveId(id) {
-      return ext.some(e => id.endsWith(e)) ? id : null;
-    },
-    transform(code, id) {
-      if (ext.some(e => id.endsWith(e))) {
-        const buffer = fs.readFileSync(id);
-        console.log(id);
-        return {
-          code: `export default ${JSON.stringify(buffer)}`,
-          map: null
-        };
-      }
-    }
-  };
-}
+// // adapted from mattjennings
+// // https://github.com/mattjennings/mattjennings.io/blob/master/vite.config.js
+// function rawFonts(ext) {
+//   return {
+//     name: 'vite-plugin-raw-fonts',
+//     resolveId(id) {
+//       return ext.some(e => id.endsWith(e)) ? id : null;
+//     },
+//     transform(code, id) {
+//       if (ext.some(e => id.endsWith(e))) {
+//         const buffer = fs.readFileSync(id);
+//         console.log(id);
+//         return {
+//           code: `export default ${JSON.stringify(buffer)}`,
+//           map: null
+//         };
+//       }
+//     }
+//   };
+// }
