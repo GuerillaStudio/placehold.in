52dfc5726f6368c75e552d3e22eef588fd5655f1
wryk <wryk@wryk.moe>
2023-07-19 18:50:05 +0200

    replace shortcode by macro

--- .eleventy.js
+++ .eleventy.js
@@ -1,40 +1,33 @@
 require('dotenv').config()
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addGlobalData("BASE_URL", process.env.URL ?? "")
+    const BASE_URL = process.env.URL ?? ""
+
+    eleventyConfig.addGlobalData("BASE_URL", BASE_URL)
     eleventyConfig.addGlobalData("SUPPORTED_FORMATS", ["png", "jpeg", "svg", "webp", "avif", "heif"])
     eleventyConfig.addGlobalData("DIMENSION_MAX", process.env.DIMENSION_MAX)
     eleventyConfig.addGlobalData("DPR_MAX", process.env.DPR_MAX)
     eleventyConfig.addGlobalData("FORMAT_DEFAULT", process.env.FORMAT_DEFAULT)
 
-    eleventyConfig.addNunjucksShortcode("placeholder", function({ width, height, dpr, format, dark, lazy }) {
-        const path = [
-            `${this.ctx.BASE_URL}/`,
+    eleventyConfig.addFilter("placeholderUrl", function({ width, height, dpr, format, dark }) {
+        return [
+            `${BASE_URL}/`,
             height ? `${width}x${height}` : width,
             dpr && `@${dpr}x`,
             format && `.${format}`,
             dark && "/dark",
         ].filter(x => x != null).join("")
+    })
 
-        const description = [
+    eleventyConfig.addFilter("placeholderDescription", function({ width, height, dpr, format, dark }) {
+        return [
             "A",
             dark && "dark",
-            height ? "rectangle" : "square",
+            height !== width ? "rectangle" : "square",
             format,
             "placeholder image",
             dpr && `scaled ${dpr} times`,
         ].filter(x => x != null).join(" ")
-
-        return `
-            <div class="not-prose">
-                <figure>
-                    <img src="${path}" width="${width}" height="${height ?? width}" alt="${description}" loading="${ lazy ? 'lazy' : 'eager' }"  fetchpriority="${ lazy ? 'low' : 'high' }" class="mx-auto text-base"  />
-                    <figcaption class="mt-2">
-                        <code class="break-all text-xs">${path}</code>
-                    </figcaption>
-                </figure>
-            </div>
-        `
     })
 
     // Copy the static assets to the output folder
